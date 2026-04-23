30749c6d2041fdcf7565014afa809d3cdf90f75c
sheep-works <sheep@mail.transheep.biz>
2026-04-23 18:02:24 +0900

    include WASM build files in the repository

---
+++ logic/pkg/sheep_spindle.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function analyze_all(tm: any, texts: any, tb: any, threshold: number, counts?: number | null): any;
+
+export function get_consistency_groups(segments: any, threshold: number): any;
+
+export function only_tb_search(texts: any, tb: any): any;
+
+export function only_tm_search(tm: any, texts: any, threshold: number, counts?: number | null): any;
