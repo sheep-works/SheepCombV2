3ab0c2c0cd96ee8644b6ef17cbd0c64481d699a4
sheep-works <sheep@mail.transheep.biz>
2026-04-23 18:07:07 +0900

    nuxt config modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@pinia/nuxt'],
   nitro: {
+    preset: 'netlify',
     devProxy: {
       '/api': {
         target: 'http://localhost:8000',
